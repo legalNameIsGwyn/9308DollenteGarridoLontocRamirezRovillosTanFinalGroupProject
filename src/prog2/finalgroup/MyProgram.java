98a6579dbaf9995e1c4166171d923cc236cf360f
legalNameIsGwyn <80401990+legalNameIsGwyn@users.noreply.github.com>
2021-05-18 17:33:37 +0800

    Update MyProgram.java

--- src/prog2/finalgroup/MyProgram.java
+++ src/prog2/finalgroup/MyProgram.java
@@ -15,33 +15,45 @@ public class MyProgram extends JFrame {
     private String source = "res/data.csv";
     private String output = "";
 
-    private JButton firstButton, secondButton, thirdButton, exitButton;
-    private JLabel text;
+    private JButton firstButton, secondButton, thirdButton, fourthButton, fifthButton, exitButton;
+    private JLabel label;
     private JFrame frame;
     private JList list1;
 
     private firstButtonHandler firstHandler;
     private secondButtonHandler secondHandler;
     private thirdButtonHandler thirdHandler;
+    private fourthButtonHandler fourthButton;
+    private fifthButtonHandler fifthButton;
     private exitButtonHandler exitHandler;
 
     private static int WIDTH = 400;
     private static int HEIGHT = 400;
 
+    /**
+     * Main menu of the program
+     */
     public MyProgram() {
-        System.out.println(MyProgramUtility.getMaxDistrict(MyProgramUtility.readFile(source)));
 
-        firstButton = new JButton("All Citizens");
+        firstButton = new JButton("Population Table");
         firstHandler = new firstButtonHandler();
         firstButton.addActionListener(firstHandler);
 
-        secondButton = new JButton("Show totals");
+        secondButton = new JButton("Statistical Data");
         secondHandler = new secondButtonHandler();
         secondButton.addActionListener(secondHandler);
 
-        thirdButton = new JButton("");
+        thirdButton = new JButton("Show Citizens by district");
         thirdHandler = new thirdButtonHandler();
         thirdButton.addActionListener(thirdHandler);
+
+        fourthButton = new JButton("fourth");
+        fourthHandler = new fourthButtonHandler();
+        fourthButton.addActionListener(fourthHandler);
+
+        fifthButton = new JButton("fifth");
+        fifthHandler = new thirdButtonHandler();
+        fifthButton.addActionListener(tfifthHandler);
 
         exitButton = new JButton("Exit");
         exitHandler = new exitButtonHandler();
@@ -50,7 +62,7 @@ public class MyProgram extends JFrame {
         frame = new JFrame();
         setTitle("Citizens");
         Container pane = getContentPane();
-        pane.setLayout(new GridLayout(5, 1));
+        pane.setLayout(new GridLayout(4, 1));
         pane.add(firstButton);
         pane.add(secondButton);
         pane.add(thirdButton);
@@ -60,28 +72,49 @@ public class MyProgram extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    /**
+     * Creates a window representing the data in a table
+     */
     private class firstButtonHandler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+            Object[][] objArray = MyProgramUtility.toObjectArray(MyProgramUtility.readFile(source));
+            String[] colNames = {"Name", "Email", "Address", "Age", "District", "Residency", "Gender"};
             JFrame firstWindowJFrame = new JFrame();
 
-            firstWindowJFrame.setSize(WIDTH,HEIGHT);
+            JTable table = new JTable(objArray, colNames);
+            JScrollPane scrollPane = new JScrollPane(table);
 
+            firstWindowJFrame.add(scrollPane);
+            firstWindowJFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            firstWindowJFrame.pack();
+            firstWindowJFrame.setVisible(true);
         }
     }
 
+    /**
+     * Shows basic statistical information of the entire population
+     */
     private class secondButtonHandler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            JLabel label;
             JFrame secondWindowJFrame = new JFrame();
             JPanel panel = new JPanel();
 
             List<Citizen> citizenList = MyProgramUtility.readFile(source);
 
             label = new JLabel("<html>Total Number of Citizens: " + MyProgramUtility.totalPopulation(citizenList)+
+                    "<br/>"+
                     "<br/>Total Number of Males: " +MyProgramUtility.getTotalMale(citizenList)+
+                    "<br/>"+
                     "<br/>Total Number of Females: " +MyProgramUtility.getTotalFemale(citizenList)+
+                    "<br/>"+
                     "<br/>Total Number of Residents: " +MyProgramUtility.numberOfResidents(citizenList)+
-                    "<br/>Total Number of Non-Residents: "+MyProgramUtility.numberOfNonResidents(citizenList)+ "</html>");
+                    "<br/>"+
+                    "<br/>Total Number of Non-Residents: "+MyProgramUtility.numberOfNonResidents(citizenList)+
+                    "<br/>"+
+                    "<br/>Number of Districts: "+ MyProgramUtility.getMaxDistrict(citizenList) +
+                    "<br/>"+
+                    "<br/>Average age of Citizens: "+ MyProgramUtility.getAverageAge(citizenList)+
+                    "</html>");
 
             setTitle("Citizens");
             Container pane = getContentPane();
@@ -97,8 +130,32 @@ public class MyProgram extends JFrame {
         }
     }
 
+    /**
+     * Shows residents categorized by district
+     */
     private class thirdButtonHandler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+            JFrame thirdWindowJFrame = new JFrame();
+            JPanel panel = new JPanel();
+            JTextField text = new JTextField();
+
+            List<Citizen> citizenList = MyProgramUtility.readFile(source);
+
+            label = new JLabel("Citizens sorted by District", SwingConstants.CENTER);
+            JScrollPane scroll = new JScrollPane(label, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+            setTitle("Citizens");
+            Container pane = getContentPane();
+            panel.setLayout(new GridLayout(1, 1));
+            panel.add(label);
+            panel.add(scroll);
+
+            thirdWindowJFrame.setTitle("Per District");
+            thirdWindowJFrame.add(panel);
+            thirdWindowJFrame.pack();
+            thirdWindowJFrame.setVisible(true);
+            thirdWindowJFrame.setSize(WIDTH, HEIGHT);
+            thirdWindowJFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         }
     }
 
