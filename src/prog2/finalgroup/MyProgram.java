8c138414e6d02a49a11a27a3c8a351a01b369ebd
KyleDTan <83455907+KyleDTan@users.noreply.github.com>
2021-05-19 13:18:55 +0800

    Update MyProgram.java

--- src/prog2/finalgroup/MyProgram.java
+++ src/prog2/finalgroup/MyProgram.java
@@ -8,6 +8,9 @@ import java.io.*;
 import java.util.Scanner;
 import java.util.List;
 
+/**
+ * MyProgram class generates the GUI for the program
+ */
 public class MyProgram extends JFrame {
 
     private Scanner kbd = new Scanner(System.in);
@@ -15,7 +18,7 @@ public class MyProgram extends JFrame {
     private String source = "res/data.csv";
     private String output = "";
 
-    private JButton firstButton, secondButton, thirdButton, fourthButton, fifthButton, exitButton;
+    private JButton firstButton, secondButton, thirdButton, fourthButton, fifthButton, sixthButton, exitButton;
     private JLabel label;
     private JFrame frame;
     private JList list1;
@@ -25,6 +28,7 @@ public class MyProgram extends JFrame {
     private thirdButtonHandler thirdHandler;
     private fourthButtonHandler fourthHandler;
     private fifthButtonHandler fifthHandler;
+    private sixthButtonHandler sixthHandler;
     private exitButtonHandler exitHandler;
 
     private static int WIDTH = 400;
@@ -43,17 +47,21 @@ public class MyProgram extends JFrame {
         secondHandler = new secondButtonHandler();
         secondButton.addActionListener(secondHandler);
 
-        thirdButton = new JButton("Show Citizens by district");
+        thirdButton = new JButton("Sort citizens by age");
         thirdHandler = new thirdButtonHandler();
         thirdButton.addActionListener(thirdHandler);
 
-        fourthButton = new JButton("fourth");
+        fourthButton = new JButton("Sort Residents");
         fourthHandler = new fourthButtonHandler();
         fourthButton.addActionListener(fourthHandler);
 
-        fifthButton = new JButton("fifth");
+        fifthButton = new JButton("Search");
         fifthHandler = new fifthButtonHandler();
         fifthButton.addActionListener(fifthHandler);
+
+        sixthButton = new JButton("Sort Non-residents");
+        sixthHandler = new sixthButtonHandler();
+        sixthButton.addActionListener(sixthHandler);
 
         exitButton = new JButton("Exit");
         exitHandler = new exitButtonHandler();
@@ -62,12 +70,13 @@ public class MyProgram extends JFrame {
         frame = new JFrame();
         setTitle("Citizens");
         Container pane = getContentPane();
-        pane.setLayout(new GridLayout(6, 1));
+        pane.setLayout(new GridLayout(4, 1));
         pane.add(firstButton);
         pane.add(secondButton);
         pane.add(thirdButton);
-        pane.add(fourthButton);
         pane.add(fifthButton);
+        pane.add(fourthButton);
+        pane.add(sixthButton);
         pane.add(exitButton);
         setSize(WIDTH, HEIGHT);
         setVisible(true);
@@ -79,17 +88,17 @@ public class MyProgram extends JFrame {
      */
     private class firstButtonHandler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            Object[][] objArray = MyProgramUtility.toObjectArray(MyProgramUtility.readFile(source));
-            String[] colNames = {"Name", "Email", "Address", "Age", "Is Resident", "District", "Gender"};
-            JFrame firstWindowJFrame = new JFrame();
+            Object[][] objArray = MyProgramUtility.toObjectArray(MyProgramUtility.sortCitizensName(MyProgramUtility.readFile(source)));
+            String[] colNames = {"Name", "Email", "Address", "Age", "Residency", "District", "Gender"};
+            JFrame firstWindowJFrame = new JFrame("Sorted Data");
 
-            JTable table = new JTable(objArray, colNames){
+            JTable table = new JTable(objArray, colNames) {
                 public boolean editCellAt(int row, int column, java.util.EventObject e) {
                     return false;
                 }
             };
-            JScrollPane scrollPane = new JScrollPane(table);
 
+            JScrollPane scrollPane = new JScrollPane(table);
             firstWindowJFrame.add(scrollPane);
             firstWindowJFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             firstWindowJFrame.pack();
@@ -107,19 +116,19 @@ public class MyProgram extends JFrame {
 
             List<Citizen> citizenList = MyProgramUtility.readFile(source);
 
-            label = new JLabel("<html>Total Number of Citizens: " + MyProgramUtility.totalPopulation(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Males: " +MyProgramUtility.getTotalMale(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Females: " +MyProgramUtility.getTotalFemale(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Residents: " +MyProgramUtility.numberOfResidents(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Non-Residents: "+MyProgramUtility.numberOfNonResidents(citizenList)+
-                    "<br/>"+
-                    "<br/>Number of Districts: "+ MyProgramUtility.getMaxDistrict(citizenList) +
-                    "<br/>"+
-                    "<br/>Average age of Citizens: "+ MyProgramUtility.getAverageAge(citizenList)+
+            label = new JLabel("<html>Total Number of Citizens: " + MyProgramUtility.totalPopulation(citizenList) +
+                    "<br/>" +
+                    "<br/>Total Number of Males: " + MyProgramUtility.getTotalMale(citizenList) +
+                    "<br/>" +
+                    "<br/>Total Number of Females: " + MyProgramUtility.getTotalFemale(citizenList) +
+                    "<br/>" +
+                    "<br/>Total Number of Residents: " + MyProgramUtility.numberOfResidents(citizenList) +
+                    "<br/>" +
+                    "<br/>Total Number of Non-Residents: " + MyProgramUtility.numberOfNonResidents(citizenList) +
+                    "<br/>" +
+                    "<br/>Number of Districts: " + MyProgramUtility.getMaxDistrict(citizenList) +
+                    "<br/>" +
+                    "<br/>Average age of Citizens: " + MyProgramUtility.getAverageAge(citizenList) +
                     "</html>");
 
             setTitle("Citizens");
@@ -131,165 +140,120 @@ public class MyProgram extends JFrame {
             secondWindowJFrame.add(panel);
             secondWindowJFrame.pack();
             secondWindowJFrame.setVisible(true);
-            secondWindowJFrame.setSize(WIDTH,HEIGHT);
+            secondWindowJFrame.setSize(WIDTH, HEIGHT);
             secondWindowJFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         }
     }
 
     /**
-     * Shows residents categorized by district
+     * Sorts the age of the citizens from youngest to oldest
      */
     private class thirdButtonHandler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            List<Citizen> citizens = MyProgramUtility.readFile(source);
-            JFrame thirdWindowFrame = new JFrame("Citizens Sorted By District");
-            thirdWindowFrame.setSize(1000, 450);
-            label = new JLabel();
-            label.setText("<html>District 1" + MyProgramUtility.sortNamesInDistrict(citizens, 1) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 2" + MyProgramUtility.sortNamesInDistrict(citizens, 2) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 3" + MyProgramUtility.sortNamesInDistrict(citizens, 3) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 4" + MyProgramUtility.sortNamesInDistrict(citizens, 4) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 5" + MyProgramUtility.sortNamesInDistrict(citizens, 5) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 6" + MyProgramUtility.sortNamesInDistrict(citizens, 6) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 7" + MyProgramUtility.sortNamesInDistrict(citizens, 7) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 8" + MyProgramUtility.sortNamesInDistrict(citizens, 8) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 9" + MyProgramUtility.sortNamesInDistrict(citizens, 9) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 10" + MyProgramUtility.sortNamesInDistrict(citizens, 10) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 11" + MyProgramUtility.sortNamesInDistrict(citizens, 11) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 12" + MyProgramUtility.sortNamesInDistrict(citizens, 12) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 13" + MyProgramUtility.sortNamesInDistrict(citizens, 13) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 14" + MyProgramUtility.sortNamesInDistrict(citizens, 14) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 15" + MyProgramUtility.sortNamesInDistrict(citizens, 15) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 16" + MyProgramUtility.sortNamesInDistrict(citizens, 16) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 17" + MyProgramUtility.sortNamesInDistrict(citizens, 17) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 18" + MyProgramUtility.sortNamesInDistrict(citizens, 18) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 19" + MyProgramUtility.sortNamesInDistrict(citizens, 19) +
-                    "<br/>" +
-                    "<br/>" +
-                    "<br/>District 20" + MyProgramUtility.sortNamesInDistrict(citizens, 20) +
-                    "</html>");
-            label.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-            JScrollPane scroll = new JScrollPane(label, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-            thirdWindowFrame.add(scroll);
-            thirdWindowFrame.setVisible(true);
+            Object[][] objArray = MyProgramUtility.toAgeSorting(MyProgramUtility.sortAge(MyProgramUtility.readFile(source)));
+            String[] colNames = {"Name", "Gender", "Age", "Email"};
+            JFrame firstWindowJFrame = new JFrame("Sorted Data for Non residents");
 
+            JTable table = new JTable(objArray, colNames) {
+                public boolean editCellAt(int row, int column, java.util.EventObject e) {
+                    return false;
+                }
+            };
+
+            JScrollPane scrollPane = new JScrollPane(table);
+            firstWindowJFrame.add(scrollPane);
+            firstWindowJFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            firstWindowJFrame.pack();
+            firstWindowJFrame.setVisible(true);
         }
     }
 
+    /**
+     * Displays the the data for residents per district
+     */
     private class fourthButtonHandler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            JFrame secondWindowJFrame = new JFrame();
-            JPanel panel = new JPanel();
+            Object[][] objArray = MyProgramUtility.toResidentOrNot(MyProgramUtility.residents(MyProgramUtility.readFile(source)));
+            String[] colNames = {"Residents", "Address", "District"};
+            JFrame firstWindowJFrame = new JFrame("Sorted Data for residents");
 
-            List<Citizen> citizenList = MyProgramUtility.readFile(source);
+            JTable table = new JTable(objArray, colNames) {
+                public boolean editCellAt(int row, int column, java.util.EventObject e) {
+                    return false;
+                }
+            };
 
-            label = new JLabel("<html>Total Number of Citizens: " + MyProgramUtility.totalPopulation(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Males: " +MyProgramUtility.getTotalMale(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Females: " +MyProgramUtility.getTotalFemale(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Residents: " +MyProgramUtility.numberOfResidents(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Non-Residents: "+MyProgramUtility.numberOfNonResidents(citizenList)+
-                    "<br/>"+
-                    "<br/>Number of Districts: "+ MyProgramUtility.getMaxDistrict(citizenList) +
-                    "<br/>"+
-                    "<br/>Average age of Citizens: "+ MyProgramUtility.getAverageAge(citizenList)+
-                    "</html>");
-
-            setTitle("Citizens");
-            Container pane = getContentPane();
-            pane.setLayout(new GridLayout(1, 1));
-            panel.add(label);
-
-            secondWindowJFrame.setTitle("Total Population");
-            secondWindowJFrame.add(panel);
-            secondWindowJFrame.pack();
-            secondWindowJFrame.setVisible(true);
-            secondWindowJFrame.setSize(WIDTH,HEIGHT);
-            secondWindowJFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            JScrollPane scrollPane = new JScrollPane(table);
+            firstWindowJFrame.add(scrollPane);
+            firstWindowJFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            firstWindowJFrame.pack();
+            firstWindowJFrame.setVisible(true);
         }
     }
 
+    /**
+     * Displays a search window where the user can search for a specific resident
+     */
     private class fifthButtonHandler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            JFrame secondWindowJFrame = new JFrame();
-            JPanel panel = new JPanel();
+            Object[][] objArray = MyProgramUtility.toObjectArray(MyProgramUtility.readFile(source));
+            String[] colNames = {"Name", "Email", "Address", "Age", "Is Resident", "District", "Gender"};
+            JTable table = new JTable(objArray, colNames) {
+                public boolean editCellAt(int row, int column, java.util.EventObject e) {
+                    return false;
+                }
+            };
+            JTextField filterField = MyProgramUtility.RowFilterUtil.createRowFilter(table);
+            JPanel jp = new JPanel();
+            jp.add(filterField);
+            frame = new JFrame("Search for Citizen");
+            frame.add(jp, BorderLayout.NORTH);
+            JScrollPane pane = new JScrollPane(table);
+            pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+            frame.add(pane, BorderLayout.CENTER);
+            frame.setVisible(true);
+            frame.setSize(1000, 700);
 
-            List<Citizen> citizenList = MyProgramUtility.readFile(source);
+        }
+    }
+    /**
+     * Displays the the data for non -residents per district
+     */
+    private class sixthButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            Object[][] objArray = MyProgramUtility.toResidentOrNot(MyProgramUtility.nonResidents(MyProgramUtility.readFile(source)));
+            String[] colNames = {"Non-Residents", "Address", "District"};
+            JFrame firstWindowJFrame = new JFrame("Sorted Data for Non residents");
 
-            label = new JLabel("<html>Total Number of Citizens: " + MyProgramUtility.totalPopulation(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Males: " +MyProgramUtility.getTotalMale(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Females: " +MyProgramUtility.getTotalFemale(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Residents: " +MyProgramUtility.numberOfResidents(citizenList)+
-                    "<br/>"+
-                    "<br/>Total Number of Non-Residents: "+MyProgramUtility.numberOfNonResidents(citizenList)+
-                    "<br/>"+
-                    "<br/>Number of Districts: "+ MyProgramUtility.getMaxDistrict(citizenList) +
-                    "<br/>"+
-                    "<br/>Average age of Citizens: "+ MyProgramUtility.getAverageAge(citizenList)+
-                    "</html>");
+            JTable table = new JTable(objArray, colNames) {
+                public boolean editCellAt(int row, int column, java.util.EventObject e) {
+                    return false;
+                }
+            };
 
-            setTitle("Citizens");
-            Container pane = getContentPane();
-            pane.setLayout(new GridLayout(1, 1));
-            panel.add(label);
-
-            secondWindowJFrame.setTitle("Total Population");
-            secondWindowJFrame.add(panel);
-            secondWindowJFrame.pack();
-            secondWindowJFrame.setVisible(true);
-            secondWindowJFrame.setSize(WIDTH,HEIGHT);
-            secondWindowJFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            JScrollPane scrollPane = new JScrollPane(table);
+            firstWindowJFrame.add(scrollPane);
+            firstWindowJFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            firstWindowJFrame.pack();
+            firstWindowJFrame.setVisible(true);
         }
     }
 
+    /**
+     * exits the program
+     */
     private class exitButtonHandler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             System.exit(0);
         }
     }
 
-
-    public static void main(String[] args) { new MyProgram(); }
+    /**
+     * Run the program
+     * @param args
+     */
+    public static void main(String[] args) {
+        new MyProgram();
     }
+}
+
